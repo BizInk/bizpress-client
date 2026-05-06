ebfeff78d27a56fba580f0d1c7e0f5b7808f0c42
Jayden Major <jayden@jaydenmajor.com>
2026-05-06 11:44:57 +1000

    Update Checking

--- bizink-client.php
+++ bizink-client.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://bizinkonline.com
  * Author: Bizink
  * Author URI: https://bizinkonline.com
- * Version: 1.9.7
+ * Version: 1.9.8
  * Requires PHP: 7.4
  * Requires at least: 6.0
  * Text Domain: bizink-client
@@ -30,7 +30,6 @@ require 'plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 $myUpdateChecker = PucFactory::buildUpdateChecker('https://github.com/BizInk/bizpress-client',__FILE__,'bizpress-client');
 $myUpdateChecker->setBranch('master');
-$myUpdateChecker->setAuthentication('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');
 
 /** Load The main plugin */
 require 'bizink-plugin.php';
